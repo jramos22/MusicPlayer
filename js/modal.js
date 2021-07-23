6e680ebfcc605d0594462f2af6193f1b2ba175ec
Kendoll Castro M <kendollcastrom.@MacBook-Pro-de-Kendoll.local>
2021-07-22 23:44:16 -0600

    Agregue el modal y su funcionamiento al proyecto, tambien sus estilos

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+function openModal() {
+    const open = document.getElementById('add__song');
+    const modal_container = document.getElementById('modal_container');
+    const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+        modal_container.classList.add('show');  
+    });
+      
+    close.addEventListener('click', () => {
+        modal_container.classList.remove('show');
+    });
+}
+
+openModal();
